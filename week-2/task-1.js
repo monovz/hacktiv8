052b1cb487ddd5370b1cc3b702873183a690d029
monovz <novritsazulfikar@gmail.com>
2020-03-23 22:30:16 +0700

    First commit, week 2 task 1

---
+++ week-2/task-1.js
@@ -0,0 +1,42 @@
+/** NOMOR 1
+ * STORE massa WITH ANY VALUE
+ * STORE percepatan WITH ANY VALUE
+ * STORE gaya WITHOUT ANY VALUE
+ * 
+ * CALCULATE massa TIMES percepatan
+ * SET gaya VALUE WITH CALCULATION RESULT
+ * 
+ * DISPLAY gaya
+ */
+
+ /**NOMOR 2
+  * STORE tahun WITH ANY VALUE
+  * 
+  * IF "tahun" mod 4 equals to 0 AND mod 100 not equal to 0
+  *     DISPLAY "Ini adalah Tahun Kabisat"
+  * ELSE IF "tahun" mod 4 equals to 0 AND mod 100 equals to 0 AND mod 400 equals to 0
+  *     DISPLAY "Ini adalah Tahun Kabisat"
+  * ElSE
+  *     DISPLAY "Ini bukan Tahun Kabisat"
+  */
+
+  /**NOMOR 3
+   * STORE jumlahPakaian WITH ANY VALUE
+   * STORE pakaianDiMesinCuci WITH 0
+   * 
+   * WHILE pakaianDiMesinCuci < jumlahPakaian
+   *    ADD pakaianDiMesinCuci by 1
+   *    DO "Pakaian anda belum masuk semua loo. Terusin terusin"
+   * 
+   * DISPLAY "Pakaian anda sudah masuk semua. Kuy dinyalain mesin cucinya. Let it do the rest." 
+   */
+
+   /**NOMOR 4
+    * STORE panjangKukuMaksimum WITH ANY VALUE
+    * 
+    * STORE panjangKukuSiswa WITH ANY VALUE
+    * IF panjangKukusiswa > panjangKukuSiswa
+    *   DO "Siswa harus dihukum. Jangan berat-berat ya, dia ga akan kuat."
+    * ELSE
+    *   DO "Siswa anak yang rajin. Puji dia, dia berhak dipuji."
+    */
