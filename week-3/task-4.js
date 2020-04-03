b99e43bb1619c0f0b6b3293185ccf141466006a9
monovz <52397802+monovz@users.noreply.github.com>
2020-04-03 22:21:52 +0700

    Update task-4.js

--- week-3/task-4.js
+++ week-3/task-4.js
@@ -1,10 +1,12 @@
 var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
 
 function dataHandling2(input){
+    //  -----------------------1. USE SPLICE------------------------------
     input.splice(1,2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung");
     input.splice(4,2, "Pria", "SMA Internasional Metro");
     console.log(input);
     
+    //  -----------------------2. SPLIT AND CALL MONTH------------------------------
     var lahirSplit=input[3].split('/');
     var bulan=lahirSplit[1];
     switch(bulan){
@@ -22,14 +24,35 @@ function dataHandling2(input){
         case '12':   {console.log('Desember'); break; }
     }
 
+    //  -----------------------3. DESCENDING SORT------------------------------
     var lahir=[];
-    for(i=lahirSplit.length-1; i>=0; i--){
+    for(i=0; i<lahirSplit.length; i++){
         lahir.push(lahirSplit[i]);
+        lahir[i]=Number(lahir[i]);
+    }
+
+    for(i=0; i<lahir.length; i++){
+        for(j=0; j<lahir.length-1; j++){
+            if(lahir[j]<lahir[j+1]){
+                var temp=lahir[j];
+                lahir[j]=lahir[j+1];
+                lahir[j+1]=temp;
+            }
+        }
+    }
+
+    for(i=0; i<lahir.length; i++){
+        if(lahir[i]<10){
+            lahir[i]='0'+lahir[i];
+        }
+        lahir[i]=lahir[i].toString();
     }
     console.log(lahir);
 
+    //  -----------------------4. JOIN WITH '-'------------------------------
     console.log(lahirSplit.join('-'))
 
+    //  -----------------------5. SET MAXIMUM CHAR FOR NAME WITH SLICE------------------------------
     var nama=input[1];
     nama=nama.slice(0,15);
     console.log(nama);
