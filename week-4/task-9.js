fffd4008ce84ddd34387f86f62bdeda60ef9f5be
monovz <52397802+monovz@users.noreply.github.com>
2020-04-11 13:25:53 +0700

    Update task-9.js

--- week-4/task-9.js
+++ week-4/task-9.js
@@ -1,14 +1,16 @@
 function checkAB(num) {
     // you can only write your code here!
     var check=false;
-    for(var i=0; i<num.length; i++){
-        if(num[i]==='a' && num[i+4]==='b' || num[i]==='b' && num[i+4]==='a'){
-            check=true;
-            break;
-        } else {continue;}
-    }
+    if(num){
+        for(var i=0; i<num.length; i++){
+            if(num[i]=='a' && num[i+4]=='b' || num[i]=='b' && num[i+4]=='a'){
+                check=true;
+                break;
+            } else {continue;}
+        }
 
-    return check;
+        return check;
+    } else {return check}
   }
   
   // TEST CASES
